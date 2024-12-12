3fd124586ad6ae64aa04561cdb70f53350311d94
Giridhar <135593477+giridhar1105@users.noreply.github.com>
2024-12-12 10:20:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -725,3 +725,64 @@ System.out.println("POSSIBLEVALUESOFeANDdARE");
 for (int i=0;i< m-1;i++)
 System.out.printf("\n%d\t%d",e[i],d[i]);
 encrypt()
+
+
+
+
+
+  10
+  Code:
+import java.util.Scanner;
+import java.lang.*; 
+public class LeakyBucket
+{
+public static void main(String[] args)
+{
+int i; 
+int a[]=new int[20]; 
+int buck_rem=0,buck_cap=4,rate=3,sent,recv; 
+Scanner in = new Scanner(System.in); 
+System.out.println("Enter the number of packets"); 
+int n = in.nextInt(); 
+System.out.println("Enter the packets"); 
+for(i=1;i<=n;i++) 
+a[i]= in.nextInt(); 
+System.out.println("Clock \t packet size \t accept \t sent \t remaining");
+for(i=1;i<=n;i++) 
+{ 
+if(a[i]!=0) 
+{
+if(buck_rem+a[i]>buck_cap)
+recv=-1;
+else
+{
+recv=a[i]; 
+buck_rem+=a[i]; 
+}
+} 
+else 
+recv=0; 
+if(buck_rem!=0) 
+{ 
+if(buck_rem<rate) 
+{
+sent=buck_rem; 
+buck_rem=0; 
+} 
+else 
+{ 
+sent=rate; 
+buck_rem=buck_rem-rate;
+}
+} 
+else 
+sent=0; 
+ if(recv==-1) 
+System.out.println(+i+ "\t\t" +a[i]+ "\t dropped \t" + sent +"\t" 
++buck_rem); 
+else System.out.println(+i+ "\t\t" +a[i] +"\t\t" +recv +"\t" +sent + "\t" 
++buck_rem); 
+} 
+} 
+
+  
